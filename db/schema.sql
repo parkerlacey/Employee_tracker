9291c2f1405d96be7c8812a067736a813cbd2713
Parker Lacey <parkerlacey2424@gmail.com>
2022-10-26 22:08:17 -0500

    fixes

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,45 @@
-DROP DATABSE IF EXISTS employee_db;
+DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
 
-CREATE TABLE department (
-  id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL
+-- CREATE TABLE department 
+DROP TABLE IF EXISTS department;
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
 );
 
-CREATE TABLE role (
-  id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INTEGER,
-  INDEX dep_ind (department_id),
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+-- CREATE TABLE role
+DROP TABLE IF EXISTS role;
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    PRIMARY KEY(id),
+    INDEX idx_department (department_id),
+    CONSTRAINT fk_role_department FOREIGN KEY (department_id)
+        REFERENCES department (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
-CREATE TABLE employee (
-  id INTEGER PRIMARY KEY AUTO_INCREMENT
-  first_name VARCHAR(30) NOT NULL, 
-  last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER,
-  INDEX role_ind (role_id) REFERENCES role(id) ON DELETE SET NULL,
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-  manager_id INTEGER,
-  INDEX manager_ind (manager_id),
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL 
+-- CREATE TABLE employee
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_emplpoyee_role FOREIGN KEY (role_id)
+        REFERENCES role (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT fk_emplpoyee_manager FOREIGN KEY (manager_id)
+        REFERENCES employee (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,76 @@
-INSERT INTO department (name)
-VALUES
-('IT'),
-('Finance & Marketing'),
-('Sales & Marketing'),
-('Operations');
+USE employee_db;
 
-INSERT INTO role (title, salary, department_id)
-VALUES
-('Full Stack Developer', 85000, 1),
-('Software Engineer', 120000, 1),
-('Accountant', 23000, 2),
-('Financial Analyst' 140000, 2),
-('Marketing Coordinator', 75000, 3),
-('Sales Lead', 93000, 3),
-('Project Manager', 110000, 4),
-('Operations Manager', 95000, 4);
+-- DEPARTMENT TABLE
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM department;
+SET SQL_SAFE_UPDATES = 1;
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+ALTER TABLE department AUTO_INCREMENT = 1;
+
+INSERT INTO department
+	(name)
 VALUES
-('Mark', 'Miller', 2, null),
-('Devin', 'Anderson', 1, 1),
-('Mary', 'Brown', 4, null),
-('Ashley', 'Jones', 3, 3),
-('Tyler', 'Moore', 6, null),
-('Ana', 'Sanchez', 5, 5),
-('Lewis', 'Allen', 7, null),
-('Katherine', 'Green', 8, 7);
+	("Board Member"),
+	("Operation"),
+	("Finance"),
+	("Sales"),
+	("HR"),
+	("Marketing"),
+	("IT"),
+	("Legal");
+
+
+-- ROLE TABLE 
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM role;
+SET SQL_SAFE_UPDATES = 1;
+ALTER TABLE role AUTO_INCREMENT = 1;
+INSERT INTO role
+	(title, salary, department_id)
+VALUES
+	("CEO", 300000, 1),
+	("CFO", 225000, 3),
+	("CMO", 200000, 6),
+	("CTO", 250000, 7),
+	("COO", 275000, 2),
+	("Legal Council", 180000, 8),
+	("Account Manager", 150000, 3),
+	("Sales Manager", 160000, 4),
+	("Salesperson", 95000, 4),
+	("Lead Engineer", 160000, 7),
+	("HR Manager", 158000, 5),
+	("Marketing Manager", 140000, 6),
+	("Accountant", 125000, 3),
+	("Legal Assistant", 100000, 8),
+	("Junior Engineer", 85000, 7),
+	("HR Admin", 87000, 5),
+	("Social Meida Admin", 75000, 6),
+	("Secretary", 65000, 2);
+
+
+
+-- EMPLOYEE TABLE 
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM employee;
+SET SQL_SAFE_UPDATES = 1;
+ALTER TABLE employee AUTO_INCREMENT = 1;
+INSERT INTO employee
+	(first_name, last_name, role_id, manager_id)
+VALUES
+	("Caleb", "Schneider", 1 , null),
+	("Rylee", "Groeneweg", 2, 1),
+	("Charlie", "Michalka", 3, 1),
+	("Will", "Indest", 4, 1),
+	("Patty", "Mills", 5, null),
+	("Ashley", "Braun", 6, 3),
+	("Benjamin", "Ostebee", 7, 3),
+	("Paige", "Lacey", 8, 2),
+	("Zachary", "Berg", 9, 4),
+	("Parker", "Lacey", 10, 1),
+	("Nick", "Thompson", 11, null),
+	("Beckham", "Lacey", 12, 1),
+	("Rebecca", "Scheier", 13,null),
+	("Josh", "Boraas", 14, 5),
+	("Seth", "Shepersky", 15,7),
+	("Vandi", "Bymers", 16,4),
+	("Bailey", "Tollefson", 17, null);
